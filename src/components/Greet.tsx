04eafacd1f4d256442fbe1ec502e009cd371d301
ksrinreddy <ksrinreddy@gmail.com>
2023-06-29 16:22:18 +0530

    get familiar typing and working with basic props

---
+++ src/components/Greet.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+// working with prop type string, number, boolean
+
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${props.messageCount} unread messages.`
+          : `Welcome Guest!`}
+      </h2>
+    </div>
+  );
+};
+
+export default Greet;
