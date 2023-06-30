8c2a0764c2bbf7f76acf7f2c582799ca99bf56b1
ksrinreddy <ksrinreddy@gmail.com>
2023-06-30 15:59:09 +0530

    type useState for initial null and subsequent non-null(user) states

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import LoggedIn from "./components/state/LoggedIn";
+import User from "./components/state/User";
 
 function App() {
   return (
     <div className="App">
-      <LoggedIn />
+      {/* <LoggedIn /> */}
+      <User />
     </div>
   );
 }
