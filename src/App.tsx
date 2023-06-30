8228d0750ee203b12dbc47557cd55f00ac985666
ksrinreddy <ksrinreddy@gmail.com>
2023-06-30 16:21:21 +0530

    type assertion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import LoggedIn from "./components/state/LoggedIn";
+
 import User from "./components/state/User";
 
 function App() {
   return (
     <div className="App">
-      {/* <LoggedIn /> */}
       <User />
     </div>
   );
--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,22 +6,26 @@ type AuthUser = {
 };
 
 const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  //   const [user, setUser] = useState<AuthUser | null>(null);
+  // type assertion
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({
       name: "Reddy",
       email: "reddy@email.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  //   const handleLogout = () => {
+  //     setUser(null);
+  //   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      {/* <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div> */}
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 };
