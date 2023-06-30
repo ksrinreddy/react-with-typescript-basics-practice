98bcd683c6eb2caffee6b746d6dc4b13d641922e
ksrinreddy <ksrinreddy@gmail.com>
2023-06-30 22:43:44 +0530

    basic typing of useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import Box from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
 import Counter from "./components/state/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/components/context/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  primary: {
+    main: "#000",
+    text: "#fff",
+  },
+  secondary: {
+    main: "#555",
+    text: "#ccc",
+  },
+};
