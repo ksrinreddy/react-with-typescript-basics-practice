734e45cf89d8d99c16fc3f0f1c746a32387e013b
ksrinreddy <ksrinreddy@gmail.com>
2023-07-02 17:46:23 +0530

    get familiar with typing polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
-import CustomComponent from "./components/html/CustomComponent";
+import Text from "./components/polymorphic/Text";
 
 function App() {
   return (
     <div className="App">
-      <CustomComponent name="ram" isLoggedIn={true} />
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
