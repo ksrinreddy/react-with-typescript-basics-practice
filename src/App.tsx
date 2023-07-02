52e0da858d1f94b22a0b89b1761f6f9f4e4dd617
ksrinreddy <ksrinreddy@gmail.com>
2023-07-02 14:18:48 +0530

    typing custom html element components(Button, Input), type Omit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
-import List from "./components/generics/List";
-import RandomNumber from "./components/restriction/RandomNumber";
+import Button from "./components/html/Button";
 
 function App() {
   return (
     <div className="App">
-      {/* <RandomNumber value={10} isPositive /> */}
-      {/* you can also set isNegative, isZero to true which is not good. you should be able to restrict that - if isPositive is set to true, other ones should be disallowed. - 'restricted props' */}
-      {/* <RandomNumber value={10} isPositive isNegative isZero /> */}
-      <RandomNumber value={10} isPositive />
+      <Button variant="primary" onClick={() => console.log("clicked")}>
+        Primary Button
+      </Button>
     </div>
   );
 }
