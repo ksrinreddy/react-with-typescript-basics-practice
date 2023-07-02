d1f64951ad841d15da9f1f8fd7dd64524cff742c
ksrinreddy <ksrinreddy@gmail.com>
2023-07-02 12:21:41 +0530

    typing generic props (to pass multiple types of props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "./App.css";
-import Private from "./components/auth/Private";
-import Profile from "./components/auth/Profile";
+import List from "./components/generics/List";
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      {/* array of strings */}
+      {/* <List
+        items={["ram", "raj", "raghav"]}
+        onClick={(item) => console.log(item)}
+      /> */}
+      {/* array of numbers */}
+      {/* <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+
+      {/* array of objects */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: "one first",
+            second: "one second",
+          },
+          {
+            id: 2,
+            first: "two first",
+            second: "two second",
+          },
+          {
+            id: 3,
+            first: "three first",
+            second: "three second",
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
